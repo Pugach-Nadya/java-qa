215963ceccffe55b75c3d2f373f549ca809a5f38
ndpugach <nadya.tiger@yandex.ru>
2023-06-19 23:20:37 +0300

    dz 1 task 1

--- src/main/java/ru/yandex/Review_HomeWork.java
+++ src/main/java/ru/yandex/Review_HomeWork.java
@@ -1,12 +1,47 @@
 package ru.yandex;
 
+import java.util.Scanner;
+
 public class Review_HomeWork {
-    public static void test ()
+    public static void reverse (int a)
     {
-        System.out.printf("Hey!");
+        int a1 = (int) a / 1000;
+        int a2 = (int) (a - (a1*1000))/100;
+        int a3 = (int) (a - (a1*1000) - (a2*100))/10;
+        int a4 = a % 10;
+        System.out.printf("Итоговое число = "+ a4 + a3 + a2 + a1);
+    }
+
+    public static boolean checkString(String st)
+    {
+        if (st == null)
+        {
+            System.out.println("Вы ввели пустую строку!");
+            return false;
+        }
+        if (st.length() != 4)
+        {
+            System.out.println("Вы ввели значение меньше 4-х символов!");
+            return false;
+        }
+        try {
+            int valueSt = Integer.parseInt(st);
+            boolean b = true;
+            return b;
+        }catch (NumberFormatException e) {
+            System.out.println("Вы ввели не число!");
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        test();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Привет, введи четерех значное число \n");
+        String st = in.nextLine();
+        boolean p = checkString(st);
+        if (p == true) {
+            int a = Integer.parseInt(st);
+            reverse(a);
+        }
     }
 }
