fdc5bec1ad697d6854d8551204206fe4eda715e5
ndpugach <nadya.tiger@yandex.ru>
2023-06-19 22:04:51 +0300

    test add

---
+++ src/main/java/ru/yandex/Review_HomeWork.java
@@ -0,0 +1,12 @@
+package ru.yandex;
+
+public class Review_HomeWork {
+    public static void test ()
+    {
+        System.out.printf("Hey!");
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
