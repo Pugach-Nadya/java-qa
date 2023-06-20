7dbe3b0ca94eeefd85e3e2d7a01f816f9607592c
ndpugach <nadya.tiger@yandex.ru>
2023-06-20 23:59:05 +0300

    dz 1 task 1 fixed of comment

--- src/main/java/ru/yandex/Review_HomeWork.java
+++ src/main/java/ru/yandex/Review_HomeWork.java
@@ -3,42 +3,32 @@ package ru.yandex;
 import java.util.Scanner;
 
 public class Review_HomeWork {
-    public static void reverse (int a)
-    {
-        int a1 = (int) a / 1000;
-        int a2 = (int) (a - (a1*1000))/100;
-        int a3 = (int) (a - (a1*1000) - (a2*100))/10;
-        int a4 = a % 10;
-        System.out.printf("Итоговое число = "+ a4 + a3 + a2 + a1);
-    }
-
-    public static boolean checkString(String st)
+    public static String reverse (String st)
     {
         if (st == null)
         {
-            System.out.println("Вы ввели пустую строку!");
-            return false;
+            String finalSt = ("Вы ввели пустую строку!");
+            return finalSt;
         }
         if (st.length() != 4)
         {
-            System.out.println("Вы ввели значение меньше 4-х символов!");
-            return false;
+            String finalSt = ("Вы ввели значение меньше 4-х символов!");
+            return finalSt;
         }
         try {
-            int valueSt = Integer.parseInt(st);
-            boolean b = true;
-            return b;
+            int a = Integer.parseInt(st);
+            int a1 = (int) a / 1000;
+            int a2 = (int) (a - (a1*1000))/100;
+            int a3 = (int) (a - (a1*1000) - (a2*100))/10;
+            int a4 = a % 10;
+            String finalSt = ("Итоговое число = "+ a4 + a3 + a2 + a1);
+            return finalSt;
         }catch (NumberFormatException e) {
-            System.out.println("Вы ввели не число!");
+            String finalSt = ("Вы ввели не число!");
+            return finalSt;
         }
-        return false;
     }
 
     public static void main(String st) {
-        boolean p = checkString(st);
-        if (p == true) {
-            int a = Integer.parseInt(st);
-            reverse(a);
-        }
     }
 }
--- src/test/java/ru/yandex/Review_HomeWorkTest.java
+++ src/test/java/ru/yandex/Review_HomeWorkTest.java
@@ -1,14 +1,35 @@
 package ru.yandex;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Review_HomeWorkTest {
     @Test
-    void TestCheckReverse4Int() {Review_HomeWork.main(String.valueOf(4789));}
+    void TestCheckReverse4Int()
+    {
+        String actually = Review_HomeWork.reverse(String.valueOf(4789));
+        String expected= "Итоговое число = 9874";
+        Assertions.assertEquals(expected, actually);
+    }
     @Test
-    void TestCheckReverse3INT() {Review_HomeWork.main(String.valueOf(789));}
+    void TestCheckReverse3INT()
+    {
+        String actually = Review_HomeWork.reverse("AA00");
+        String expected= "Вы ввели не число!";
+        Assertions.assertEquals(expected, actually);
+    }
     @Test
-    void TestCheckReverse4NotInt() {Review_HomeWork.main("abcd");}
+    void TestCheckReverse4NotInt()
+    {
+        String actually = Review_HomeWork.reverse(String.valueOf(789));
+        String expected= "Вы ввели значение меньше 4-х символов!";
+        Assertions.assertEquals(expected, actually);
+    }
     @Test
-    void TestCheckReverseNull() {Review_HomeWork.main(null);}
+    void TestCheckReverseNull()
+    {
+        String actually = Review_HomeWork.reverse(null);
+        String expected= "Вы ввели пустую строку!";
+        Assertions.assertEquals(expected, actually);
+    }
 }
