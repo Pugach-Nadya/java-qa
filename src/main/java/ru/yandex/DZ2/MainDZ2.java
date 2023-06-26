59c9dc6fc72c43633f9913b34b6ae9b576379dca
ndpugach <nadya.tiger@yandex.ru>
2023-06-26 14:48:46 +0300

    dz 2 version error

--- src/main/java/ru/yandex/DZ2/MainDZ2.java
+++ src/main/java/ru/yandex/DZ2/MainDZ2.java
@@ -7,9 +7,12 @@ package ru.yandex.DZ2;
 // x(int), y(int), z(int), zone(String)
 //* Sku: serialKey(long), storerKey(long), description(String)
 
+//очень смущает что мы везде целиком в SerialInventory прокидываем объекты - у нас serialKey повторяется
+
 public class MainDZ2 {
   //так и не поняла почему ему так не нравиться создание нового обьекта, баз нью тоже ругается(
   SKU zSku = new SKU().withSerialKey(1234).withStorerKey(1234).withDescription("Test").bulder();
+  //при чем судя ао всему ошибка на всех класах
   Loc zLoc = new Loc().withSerialKey(zSku).withName("1234")
           .withLength(10).withHeight(20).withHeight(18)
           .withX(11).withY(11).withZ(0).withZone("SORT").bulder();
